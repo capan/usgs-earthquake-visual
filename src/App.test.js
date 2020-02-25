5c4eb33f5ac3747702ba0805e649e39bc6398d49
capan <capanh@gmail.com>
2020-02-25 17:52:42 +0300

    update test

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import 'jest-canvas-mock';
 import App from './App';
 
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  Map: () => ({}),
+}));
 test('renders learn react link', () => {
-  window.URL.createObjectURL = jest.fn();
-  afterEach(() => {
-    window.URL.createObjectURL.mockReset();
-  });
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
