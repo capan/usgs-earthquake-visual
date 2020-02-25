524d75c9df3bb7d6c3d69324b25d558834027cbe
capan <capanh@gmail.com>
2020-02-25 17:21:11 +0300

    jest test problem fix

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,10 @@ import { render } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
+  window.URL.createObjectURL = jest.fn();
+  afterEach(() => {
+    window.URL.createObjectURL.mockReset();
+  });
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
