793a7260a443c45985369bbf275b863fa0aceb10
capan <capanh@gmail.com>
2020-02-25 21:58:05 +0300

    docs file created

---
+++ docs/precache-manifest.eafbfe7d1e50269dc2a50bace14a3cfc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcbad4b9dede4b19d89af07ed6bb79e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "59a2bca4290ce4f09a32",
+    "url": "/static/css/2.f37a2784.chunk.css"
+  },
+  {
+    "revision": "ca26916650bbd9fccfbb",
+    "url": "/static/css/main.5bcd4307.chunk.css"
+  },
+  {
+    "revision": "59a2bca4290ce4f09a32",
+    "url": "/static/js/2.f0a2b066.chunk.js"
+  },
+  {
+    "revision": "b924938c2286f24c4da3fb33ab07f7f6",
+    "url": "/static/js/2.f0a2b066.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca26916650bbd9fccfbb",
+    "url": "/static/js/main.b1fac758.chunk.js"
+  },
+  {
+    "revision": "d350fd846c27d9ab1d2c",
+    "url": "/static/js/runtime-main.14a19222.js"
+  }
+]);
