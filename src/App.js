aa136bb1e7d0dcad96df07a187b687a47c5c1a81
capan <capanh@gmail.com>
2020-10-10 00:23:16 +0300

    feat: mobile device maintance page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 /* eslint-disable react/destructuring-assignment */
-import React, { Component } from 'react';
-import ReactMapboxGl, { GeoJSONLayer, Cluster, Marker } from 'react-mapbox-gl';
-import * as MapboxGL from 'mapbox-gl';
-import Lottie from 'react-lottie';
-import './App.css';
-import Geocoder from 'react-mapbox-gl-geocoder';
-import SplitScreen from './Components/SplitScreen';
-import EventCardsHolder from './Components/EventCardsHolder';
-import EventCard from './Components/EventCard';
-import * as animationData from './Extras/loading.json';
-import RangeSlider from './Components/RangeSlider';
-import Switcher from './Components/Switcher';
-import ButtonAppBar from './Components/Navbar';
-import EQTicker from './Components/Ticker';
+import React, { Component } from "react";
+import ReactMapboxGl, { GeoJSONLayer, Cluster, Marker } from "react-mapbox-gl";
+import * as MapboxGL from "mapbox-gl";
+import Lottie from "react-lottie";
+import "./App.css";
+import Geocoder from "react-mapbox-gl-geocoder";
+import SplitScreen from "./Components/SplitScreen";
+import EventCardsHolder from "./Components/EventCardsHolder";
+import EventCard from "./Components/EventCard";
+import * as animationData from "./Extras/loading.json";
+import RangeSlider from "./Components/RangeSlider";
+import Switcher from "./Components/Switcher";
+import ButtonAppBar from "./Components/Navbar";
+import EQTicker from "./Components/Ticker";
 
 const Map = ReactMapboxGl({
   accessToken: process.env.REACT_APP_MAPBOX_ACCESS_TOKEN,
@@ -31,12 +31,14 @@ class App extends Component {
       geojsonData: null,
       dragSearch: true,
       bounds: undefined,
+      screenWidth: 0,
     };
     this.mapRef = React.createRef();
 
     // mapbox-gl doesn't work with Id's in string. We should hash the incoming id from the API
     // eslint-disable-next-line no-bitwise
-    this.hashCode = (s) => s.split('').reduce((a, b) => ((a << 5) - a + b.charCodeAt(0)) | 0, 0);
+    this.hashCode = (s) =>
+      s.split("").reduce((a, b) => ((a << 5) - a + b.charCodeAt(0)) | 0, 0);
     this.requestMaker = this.requestMaker.bind(this);
     this.onSelected = this.onSelected.bind(this);
     this.onCardMouseOut = this.onCardMouseOut.bind(this);
@@ -47,6 +49,7 @@ class App extends Component {
     this.cardClickHandler = this.cardClickHandler.bind(this);
     this.onStyleLoadHandler = this.onStyleLoadHandler.bind(this);
     this.sliderChangeHandler = this.sliderChangeHandler.bind(this);
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
     this.earthQuakeHoverHandler = this.earthQuakeHoverHandler.bind(this);
     this.paginationChangeHandler = this.paginationChangeHandler.bind(this);
     this.tickData = undefined;
@@ -55,7 +58,7 @@ class App extends Component {
       autoplay: true,
       animationData: animationData.default,
       rendererSettings: {
-        preserveAspectRatio: 'xMidYMid slice',
+        preserveAspectRatio: "xMidYMid slice",
       },
     };
   }
@@ -81,7 +84,17 @@ class App extends Component {
   }
 
   componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener("resize", this.updateWindowDimensions);
     this.requestMaker(this.mapRef, this.state.startDate, this.state.endDate);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ screenWidth: window.innerWidth });
   }
 
   onSelected(viewport) {
@@ -118,8 +131,8 @@ class App extends Component {
   onCardMouseOver(id) {
     if (id) {
       this.mapRef.state.map.setFeatureState(
-        { source: 'mygeolayer', id },
-        { hover: true },
+        { source: "mygeolayer", id },
+        { hover: true }
       );
     }
   }
@@ -127,8 +140,8 @@ class App extends Component {
   onCardMouseOut(id) {
     if (id) {
       this.mapRef.state.map.setFeatureState(
-        { source: 'mygeolayer', id },
-        { hover: false },
+        { source: "mygeolayer", id },
+        { hover: false }
       );
     }
   }
@@ -160,32 +173,33 @@ class App extends Component {
           startDate,
           endDate,
         },
-        () => this.requestMaker(
+        () =>
+          this.requestMaker(
             this.mapRef,
             this.state.startDate,
-            this.state.endDate,
-          ),
+            this.state.endDate
+          )
       );
     }
   }
 
   earthQuakeHoverHandler(e) {
     if (
-      e.type === 'mouseenter'
-      && e.features
-      && !e.features[0].properties.cluster
+      e.type === "mouseenter" &&
+      e.features &&
+      !e.features[0].properties.cluster
     ) {
       this.setState({
         hoveredEQId: this.hashCode(
-          e.features[0].properties.net + e.features[0].properties.code,
+          e.features[0].properties.net + e.features[0].properties.code
         ),
       });
       const el = document.getElementById(this.state.hoveredEQId);
-      el.style.setProperty('box-shadow', '10px 10px 5px #459880');
-    } else if (e.type === 'mouseleave') {
+      el.style.setProperty("box-shadow", "10px 10px 5px #459880");
+    } else if (e.type === "mouseleave") {
       if (this.state.hoveredEQId) {
         const el = document.getElementById(this.state.hoveredEQId);
-        el.style.removeProperty('box-shadow');
+        el.style.removeProperty("box-shadow");
       }
     }
   }
@@ -211,46 +225,46 @@ class App extends Component {
 
   markerGenerator(coordinateList) {
     MapboxGL.SymbolLayout = {
-      'text-field': '{place}',
-      'text-font': ['Open Sans Semibold', 'Arial Unicode MS Bold'],
-      'text-offset': [0, 0.6],
-      'text-anchor': 'top',
+      "text-field": "{place}",
+      "text-font": ["Open Sans Semibold", "Arial Unicode MS Bold"],
+      "text-offset": [0, 0.6],
+      "text-anchor": "top",
     };
     const symbolLayout = MapboxGL.SymbolLayout;
     MapboxGL.SymbolPaint = {
-      'text-color': 'black',
+      "text-color": "black",
     };
     const symbolPaint = MapboxGL.SymbolPaint;
     MapboxGL.CircleLayout = {
-      visibility: 'visible',
+      visibility: "visible",
     };
     const circleLayout = MapboxGL.CircleLayout;
     MapboxGL.CirclePaint = {
-      'circle-color': [
-        'case',
-        ['boolean', ['feature-state', 'hover'], false],
-        '#459880',
-        'rgba(255, 0, 0, 0.3)',
+      "circle-color": [
+        "case",
+        ["boolean", ["feature-state", "hover"], false],
+        "#459880",
+        "rgba(255, 0, 0, 0.3)",
       ],
-      'circle-radius': [
-        'interpolate',
-        ['linear'],
-        ['zoom'],
+      "circle-radius": [
+        "interpolate",
+        ["linear"],
+        ["zoom"],
         0,
-        ['get', 'mag'],
+        ["get", "mag"],
         22,
-        ['*', 11, ['get', 'mag']],
+        ["*", 11, ["get", "mag"]],
       ],
     };
     const circlePaint = MapboxGL.CirclePaint;
     const geojsonObject = {
-      type: 'FeatureCollection',
+      type: "FeatureCollection",
       features: [],
     };
     for (let i = 0; i < coordinateList.features.length; i += 1) {
       geojsonObject.features.push(coordinateList.features[i]);
       geojsonObject.features[i].id = this.hashCode(
-        coordinateList.features[i].id,
+        coordinateList.features[i].id
       );
     }
     this.setState({
@@ -294,26 +308,27 @@ class App extends Component {
       ));
     }
 
-    const clusterMarker = (coordinates, pointCount) => (this.state.geojsonData ? (
+    const clusterMarker = (coordinates, pointCount) =>
+      this.state.geojsonData ? (
         <Marker coordinates={coordinates} anchor="center">
           <span
             style={{
-              height: '30px',
-              width: '30px',
-              backgroundColor: 'orange',
-              borderRadius: '50%',
-              display: 'inline-block',
-              textAlign: 'center',
-              pointerEvents: 'none',
-              paddingTop: '5px',
+              height: "30px",
+              width: "30px",
+              backgroundColor: "orange",
+              borderRadius: "50%",
+              display: "inline-block",
+              textAlign: "center",
+              pointerEvents: "none",
+              paddingTop: "5px",
             }}
           >
-            <b>{pointCount > 100 ? '100+' : pointCount}</b>
+            <b>{pointCount > 100 ? "100+" : pointCount}</b>
           </span>
         </Marker>
       ) : (
-        ''
-      ));
+        ""
+      );
 
     const cluster = this.state.geojsonData ? (
       <Cluster ClusterMarkerFactory={clusterMarker}>
@@ -328,9 +343,9 @@ class App extends Component {
         ))}
       </Cluster>
     ) : (
-      ''
+      ""
     );
-    return (
+    return this.state.screenWidth > 800 ? (
       <div>
         <ButtonAppBar />
         <div>
@@ -364,7 +379,7 @@ class App extends Component {
                 />
               )
             }
-            rightPane={(
+            rightPane={
               <div>
                 <div className="flex-container">
                   <div className="slider">
@@ -372,7 +387,9 @@ class App extends Component {
                   </div>
                   <div className="switcher">
                     <Switcher
-                      onSwitcherChanged={(state) => this.switcherChangeHandler(state)}
+                      onSwitcherChanged={(state) =>
+                        this.switcherChangeHandler(state)
+                      }
                     />
                   </div>
                   <div className="searchbox">
@@ -395,22 +412,54 @@ class App extends Component {
                   center={this.state.mapCenter}
                   zoom={this.state.mapZoom}
                   containerStyle={{
-                    height: '85vh',
-                    width: '75vw',
+                    height: "85vh",
+                    width: "75vw",
                   }}
                   // maxBounds={[[19, 29], [46, 50]]}
                   onDragEnd={this.onDragEndHandler}
                   onZoomEnd={this.onZoomEndHandler}
                 >
                   {this.state.geojsonLayer}
-                  {this.state.geojsonData ? cluster : ''}
+                  {this.state.geojsonData ? cluster : ""}
                 </Map>
               </div>
-            )}
+            }
           />
         </div>
         <EQTicker jsonData={this.state.geojsonLayer} />
       </div>
+    ) : (
+      <div
+        dangerouslySetInnerHTML={{
+          __html: `<!DOCTYPE html>
+          <html>
+              <head>
+                  <title>Site is down for maintenance</title>
+                  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+                  <meta name="viewport" content="width=device-width, initial-scale=1" />
+                  <style type="text/css">
+                      body { text-align: center; padding: 10%; font: 20px Helvetica, sans-serif; color: #333; }
+                      h1 { font-size: 50px; margin: 0; }
+                      article { display: block; text-align: left; max-width: 650px; margin: 0 auto; }
+                      a { color: #dc8100; text-decoration: none; }
+                      a:hover { color: #333; text-decoration: none; }
+                      @media only screen and (max-width : 480px) {
+                          h1 { font-size: 40px; }
+                      }
+                  </style>
+              </head>
+              <body>
+                  <article>
+                      <h1>This app is not mobile compatible yet.</h1>
+                      <p>Please check back soon.</p>
+                      <p>We apologize for any inconvenience.</p>
+                      <img src="https://media0.giphy.com/media/7TtvTUMm9mp20/giphy.webp?cid=ecf05e47uq96gncozmeougz43cimvlyzb6am37154hbiucl6&rid=giphy.webp" alt="terminator ok"/>
+                      <p id="signature">&mdash; <a href="mailto:[capanh@gmail.com]">[Huseyin]</a></p>
+                  </article>
+              </body>
+          </html>`,
+        }}
+      />
     );
   }
 }
